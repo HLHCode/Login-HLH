d1436c84ad464221945b7911d535354520f0f8cf
HLHCode <147956114+HLHCode@users.noreply.github.com>
2024-07-28 20:09:02 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#login-form').on('submit', function(event) {
+        event.preventDefault();
+        const username = $('#username').val();
+        const password = $('#password').val();
+
+        if (username === "HLH" && password === "Hlh0220112345*") {
+            window.location.href = "https://hlhcode.github.io/Calculadora-de-Fibonacci/";
+        } else {
+            $('#error-message').show();
+        }
+    });
+});
